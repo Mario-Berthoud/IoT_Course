42bb6fef94836254a00d300e7ecdca92fc85c734
Mario-Berthoud <mario-berthoud@outlook.com>
2024-10-31 12:19:50 +0100

    Project Added

---
+++ Template-Bath/Template/main/main.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+//#define EXAMPLE_ESP_WIFI_SSID      "CAPS-Seminar-Room"
+//#define EXAMPLE_ESP_WIFI_PASS      "caps-schulz-seminar-room-wifi"
+//#define SNTP_SERVER_NAME           "ntp1.in.tum.de"
+
+#define EXAMPLE_ESP_WIFI_SSID      "SSN_AP_Tr9ANH"
+#define EXAMPLE_ESP_WIFI_PASS      "ykKTzYzHnt9F"
+#define SNTP_SERVER_NAME           "pool.ntp.org"
+
+#define MQTT_BROKER                "192.168.1.132" 
+
+#define DEVICE_ID                  "2"
+#define DEVICE_TOPIC               "1/2/data"
+#define DEVICE_KEY                 "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MzAzMzE0NzAsImlzcyI6ImlvdHBsYXRmb3JtIiwic3ViIjoiMS8zIn0.kIJnIR0AvzkJYmMXJ2wGPVqW662gieaW19lFSd8qx_c6bi6JiUiEfEMCnGr9G5sciwGit5yzyqMZtWHzJA0ii3G-18iHvOImcrFzL6NLjayDBBgpH7isXXd2XB8Dq3brKj4Mkgs2phBnyasOXtlvH42Hj88fi1I4LQeC3oE1LN9v6nN1Nbr3Va3Dv0MNUbh2NFvEp2VTLWIQ-mXSVRy25HXlyNkJ-5UyWFuLyFkxK1Cn9RtgQj08B9omyouD-vQtSf4imLrVEbDpqDherJ3EGBnYk8xl09tt4WmXBg5GdBk3YsM04uZhSTpnLvusqJkFi2vjBXK7P8DBF_-W0nqSjJHEMyq2hnpogb8K-zxqLL2rF_eqVd5KdmGjKv5OgGxFrkWMPWTd_eBCwrysm5fUCGW5h7gV2cSD0ZwACKcoa_ltihh3lpHr2SvPKD-K-wlindzSpQwUVx-ZAaI8b0oSOFDqjVqVkpYzaFN9olwc9yKUvvgBxcIzVLvgtDJ5SpN_Uw5dAQ-8jQ30NvmHoSEIYQSaHGdqgOdAaYf9TnEcFPx9iB8Q1IbqGVC-t8s6uaiby8aD4shPQi5k6kg2jiHIu5IkFX4VKl84NrtkZCQpm_pf3DcMGcFNC4pqOWYKbJC0_HM0QsIF6qWsweCwMRpb0lzAxfhd08ldXJisWpv97Kc"
+#define PIR_PIN 27 
---
+++ Template-Corridor/Template/main/main.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+//#define EXAMPLE_ESP_WIFI_SSID      "CAPS-Seminar-Room"
+//#define EXAMPLE_ESP_WIFI_PASS      "caps-schulz-seminar-room-wifi"
+//#define SNTP_SERVER_NAME           "ntp1.in.tum.de"
+
+#define EXAMPLE_ESP_WIFI_SSID      "SSN_AP_Tr9ANH"
+#define EXAMPLE_ESP_WIFI_PASS      "ykKTzYzHnt9F"
+#define SNTP_SERVER_NAME           "pool.ntp.org"
+
+#define MQTT_BROKER                "192.168.1.132" 
+
+#define DEVICE_ID                  "2"
+#define DEVICE_TOPIC               "1/2/data"
+#define DEVICE_KEY                 "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MzAzMjk3OTcsImlzcyI6ImlvdHBsYXRmb3JtIiwic3ViIjoiMS8yIn0.rxYvyObaG2jm5yUApyQedVc7JvC3zY5BWjyj--5LgPyz-6f_1PloApKW3reVtQ3sgWgOZOqPFZv4-pMvS2LBgj2N_ZjvDaYZftWf4mgRid9FH3jSdvfthZrO_9oJOIQ7ayZm_3Er5EIUlPDDhd5M8m1-CCwjuIp5_M4M4ASkcH9zj0eylrMIXT_5aQZjVcm7rcWpMQKcs7gHpXP8KBUQSFTtkjYo0oJL_dyKIdP2QdUPzGAjKyON3joVygDpG2BADAMRE7nt6O3sDhjrOuGpLCNFem-M38M_7nJ83KVlsp3OH_4gT4zFSJEBzYp1HrQb1-98XGcA41hoagrQPP6zoLlzz3YGBfFS5RSapfg-B4gE3F0ZEoaOTcO27YOqBR35IluDGW_BIoOCt_11WW2WQ5gNtaSesDXZMpY-6sI_cepuukPsdMetVxc9cKuEr6AtZIPkHkxa5prtv1X699bAPJOgY5NBUtRwc41Z-l5I-AWCWYdgw2haAVRsKqs_eIoIBqA73pzqdKbJLglcueKdhaM8-trpSx941-8kuDu95s6aSnlSHdIix0Ejb4BYDs1yUdLnwhsEDwD_WyO8Z1ro1p080WYc5YpZUCWUGhitqQBx0CEKMyeSMkxMfO2SdgitFH5b1VY9XElRnz2_3ARraYeOtcHB2zdcQ4TBNditu3Q"
+
+
+#define PIR_PIN 27 
